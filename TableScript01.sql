cfd27a4bc306998ab32a3d619bae50fa66af8615
ZalanM <raynor@freemail.hu>
2016-04-24 14:56:20 +0200

    Revert "ApoloWindow"
    This reverts commit f15bbae6384ae425455fff9c68a4110b3596bd36.

--- TableScript01.sql
+++ TableScript01.sql
@@ -8,7 +8,6 @@ create table KiadottGyogyszer(
 	Datum datetime,
 	Deleted tinyint
 )
-
 IF OBJECT_ID('Gyogyszer') IS NOT NULL drop table Gyogyszer
 create table Gyogyszer(
 	GyogyszerID int identity(1,1) primary key,
@@ -19,7 +18,6 @@ create table Gyogyszer(
 	EgysegMennyiseg int,
 	Deleted tinyint
 )
-
 IF OBJECT_ID('Lazlap') IS NOT NULL drop table Lazlap
 create table Lazlap(
 	LazlapID int identity(1,1) primary key,
@@ -48,7 +46,6 @@ create table KorhaziEszkoz(
 	Statusz bit,
 	Deleted tinyint
 )
-
 IF OBJECT_ID('Idopontok') IS NOT NULL drop table Idopontok
 create table Idopontok(
 	IdopontID int identity(1,1) primary key,
@@ -57,12 +54,9 @@ create table Idopontok(
 	Datum datetime,
 	Deleted tinyint,
 )
-
 IF OBJECT_ID('ApoloMuszak') IS NOT NULL drop table ApoloMuszak
 create table ApoloMuszak(
 	ApoloMuszakID int identity(1,1) primary key,
 	PeopleID int,
-	StartDate datetime,
-	EndDate datetime,
-	Deleted tinyint
+	Datum datetime
 )
