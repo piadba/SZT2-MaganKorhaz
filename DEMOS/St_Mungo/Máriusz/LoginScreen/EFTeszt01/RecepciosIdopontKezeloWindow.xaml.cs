c7fd9b443e267c69f69b0965855367c18a66ca1d
ZalanM <raynor@freemail.hu>
2016-04-29 11:58:26 +0200

    valami nem volt ok a repoval.,..

---
+++ DEMOS/St_Mungo/Máriusz/LoginScreen/EFTeszt01/RecepciosIdopontKezeloWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.Entity;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EFTeszt01
+{
+    /// <summary>
+    /// Interaction logic for IdopontKezelo.xaml
+    /// </summary>
+    public partial class RecepciosIdopontKezeloWindow : Window
+    {
+        MungoSystem mungoSystem;
+        RecepciosViewModel recepciosViewModel;
+        public RecepciosIdopontKezeloWindow(RecepciosViewModel recepciosViewModel)
+        {
+            InitializeComponent();
+            this.recepciosViewModel = recepciosViewModel;
+
+            mungoSystem = recepciosViewModel.MungoSystem;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+           
+            mungoSystem.People.Load();
+       
+            this.DataContext = recepciosViewModel;
+            recepciosViewModel.Orvosok.Clear();
+            var orvosadatok = mungoSystem.People.Local.Where(x => x.Group == 2 && x.Deleted==0);
+            
+            foreach (var p in orvosadatok)
+            {
+                recepciosViewModel.Orvosok.Add(p);
+            }
+
+           
+            
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            mungoSystem.Idopontok.Load();
+           
+            People o = comboBox.SelectedItem as People;
+            var idopontok = mungoSystem.Idopontok.Local.Where
+                (x => x.Deleted == 0 && o.Deleted == 0 && x.OrvosID == o.PeopleID && x.Datum.Value.ToShortDateString() ==datePicker.SelectedDate.Value.ToShortDateString());
+            recepciosViewModel.Idopontok.Clear();
+
+            var idopontadatok = from i in idopontok
+                                join b in mungoSystem.Betegek on i.BetegID equals b.BetegID
+                                join p in mungoSystem.People on b.PeopleID equals p.PeopleID
+                                where i.Deleted == 0 && b.Deleted == 0 && p.Deleted == 0
+                                select new { Nev = p.Name, IdopontID = i.IdopontID, Datum = i.Datum, TAJ = b.TAJ };
+
+
+            recepciosViewModel.IdopontAdatok.Clear();
+
+            foreach (var p in idopontadatok)
+                recepciosViewModel.IdopontAdatok.Add(new IdopontIDBeteg { Nev = p.Nev, TAJ = p.TAJ, IdopontID = p.IdopontID, Datum = (DateTime)p.Datum });
+
+            foreach (var i in idopontok)
+            {
+                if (i.BetegID == null)
+                    recepciosViewModel.IdopontAdatok.Add(new IdopontIDBeteg { IdopontID = i.IdopontID, Datum = (DateTime)i.Datum, Nev = "", TAJ = "" });
+            }
+
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine(listBox.SelectedItem);
+            IdopontIDBeteg selected = (listBox.SelectedItem) as IdopontIDBeteg;
+            Idopontok ip = mungoSystem.Idopontok.Where(x => x.IdopontID == selected.IdopontID).First();
+            RecepciosIdopontSzerkesztoWindow isz = new RecepciosIdopontSzerkesztoWindow(recepciosViewModel, ip);
+           
+            isz.ShowDialog();
+ 
+        }
+
+
+    }
+
+
+
+
+
+}
