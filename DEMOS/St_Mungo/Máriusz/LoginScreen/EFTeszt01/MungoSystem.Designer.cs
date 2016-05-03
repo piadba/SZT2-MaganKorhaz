6b69920ada8feb58732e689b9b62e05722113e6a
soranh <soranh@gmail.com>
2016-05-03 12:27:17 +0200

    localDB !

--- DEMOS/St_Mungo/Máriusz/LoginScreen/EFTeszt01/MungoSystem.Designer.cs
+++ DEMOS/St_Mungo/Máriusz/LoginScreen/EFTeszt01/MungoSystem.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\andras\Documents\GitHub\ujclone\SZT2-MaganKorhaz\DEMOS\St_Mungo\Máriusz\LoginScreen\EFTeszt01\MungoSystem.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\andras\Documents\GitHub\work0503\St_Mungo\Máriusz\LoginScreen\EFTeszt01\MungoSystem.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
